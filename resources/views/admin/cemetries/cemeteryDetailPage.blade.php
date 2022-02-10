70696394af39eeb5a0e832449b99eb00b563d385
1986dheeraj <93111832+1986dheeraj@users.noreply.github.com>
2022-02-10 22:31:51 +0530

    revert old changes

--- resources/views/admin/cemetries/cemeteryDetailPage.blade.php
+++ resources/views/admin/cemetries/cemeteryDetailPage.blade.php
@@ -10,7 +10,7 @@
   <div class="mapinner">
     <iframe
       src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d14007.216583004956!2d76.99848779601191!3d28.63563147918303!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1642699357526!5m2!1sen!2sin"
-      width="600" height="450" style="border:0;position:fixed"  allowfullscreen="" loading="lazy"></iframe>
+      width="600" height="450" style="border:0;"  allowfullscreen="" loading="lazy"></iframe>
     <a class="btn btn-primary mflistbtn">
       <i class="fa fa-chevron-right"></i>
     </a>
@@ -73,7 +73,7 @@
                   <span class="tagtxt">Outdoor</span>
                   <i class="far fa-heart"></i>
                   <div class="boxinner_img">
-                    <img src="http://127.0.0.1:8000/newPublic/images/cemlist-img1.jpg" alt="">
+                    <img src="{{ asset('newPublic/images/cemlist-img1.jpg') }}" alt="">
                   </div>
                   <div class="boxinnercnt">
                     <h2 class="bxhead mt-0">Arbor Memorial Glen Oaks Cemetery</h2>
@@ -90,7 +90,7 @@
                   <span class="tagtxt">Outdoor</span>
                   <i class="far fa-heart"></i>
                   <div class="boxinner_img ">
-                    <img src="http://127.0.0.1:8000/newPublic/images/cemlist-img1.jpg" alt="">
+                    <img src="{{ asset('newPublic/images/cemlist-img1.jpg') }}" alt="">
                   </div>
                   <div class="boxinnercnt">
                     <h2 class="bxhead mt-0">Arbor Memorial Glen Oaks Cemetery</h2>
@@ -107,7 +107,7 @@
                   <span class="tagtxt">Outdoor</span>
                   <i class="far fa-heart"></i>
                   <div class="boxinner_img">
-                    <img src="http://127.0.0.1:8000/newPublic/images/cemlist-img1.jpg" alt="">
+                    <img src="{{ asset('newPublic/images/cemlist-img1.jpg') }}" alt="">
                   </div>
                   <div class="boxinnercnt">
                     <h2 class="bxhead mt-0">Arbor Memorial Glen Oaks Cemetery</h2>
@@ -123,7 +123,7 @@
                   <span class="tagtxt">Outdoor</span>
                   <i class="far fa-heart"></i>
                   <div class="boxinner_img">
-                    <img src="http://127.0.0.1:8000/newPublic/images/cemlist-img1.jpg" alt="">
+                    <img src="{{ asset('newPublic/images/cemlist-img1.jpg') }}" alt="">
                   </div>
                   <div class="boxinnercnt">
                     <h2 class="bxhead mt-0">Arbor Memorial Glen Oaks Cemetery</h2>
@@ -138,7 +138,7 @@
                   <span class="tagtxt">Outdoor</span>
                   <i class="far fa-heart"></i>
                   <div class="boxinner_img">
-                    <img src="http://127.0.0.1:8000/newPublic/images/cemlist-img1.jpg" alt="">
+                    <img src="{{ asset('newPublic/images/cemlist-img1.jpg') }}" alt="">
                   </div>
                   <div class="boxinnercnt">
                     <h2 class="bxhead mt-0">Arbor Memorial Glen Oaks Cemetery</h2>
@@ -153,7 +153,7 @@
                   <span class="tagtxt">Outdoor</span>
                   <i class="far fa-heart"></i>
                   <div class="boxinner_img">
-                    <img src="http://127.0.0.1:8000/newPublic/images/cemlist-img1.jpg" alt="">
+                    <img src="{{ asset('newPublic/images/cemlist-img1.jpg') }}" alt="">
                   </div>
                   <div class="boxinnercnt">
                     <h2 class="bxhead mt-0">Arbor Memorial Glen Oaks Cemetery</h2>
@@ -205,7 +205,7 @@
 </div>
 
       @include('partials.outerDetailModal')
-<footer class="adm_footer">
+<!--<footer class="adm_footer">
             <div class="container-lg">
                 <div class="row pt-md-5" align="right">
                 <div class="col-lg-12 col-12">
@@ -277,6 +277,6 @@
 
                 </div>
             </div>
-        </footer>
+        </footer>-->
 
 @endsection
--- resources/views/admin/cemetries/cemeteryListView.blade.php
+++ resources/views/admin/cemetries/cemeteryListView.blade.php
@@ -9,7 +9,7 @@
           <li><i class="fa fa-list-ul" aria-hidden="true"></i> List</li>
         </ul>
         <div class="mapinner">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d14007.216583004956!2d76.99848779601191!3d28.63563147918303!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1642699357526!5m2!1sen!2sin" width="600" height="450" style="border:0;position:fixed" allowfullscreen="" loading="lazy"></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d14007.216583004956!2d76.99848779601191!3d28.63563147918303!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1642699357526!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
             <a class="btn btn-primary mflistbtn">
              <i class="fa fa-chevron-right"></i>
             </a>
@@ -336,7 +336,7 @@
       
       
       @include('partials.outerDetailModal')
-      <footer class="adm_footer">
+     <!-- <footer class="adm_footer">
             <div class="container-lg">
                 <div class="row pt-md-5" align="right">
                 <div class="col-lg-12 col-12">
@@ -408,6 +408,6 @@
 
                 </div>
             </div>
-        </footer>
+        </footer>-->
 
 @endsection
