67496a08c1032de4cda887385b30b510fca54d24
1986dheeraj <93111832+1986dheeraj@users.noreply.github.com>
2022-02-16 12:44:46 +0530

    Add 360 Image design issue resolved

--- resources/views/admin/cemetries/new.blade.php
+++ resources/views/admin/cemetries/new.blade.php
@@ -72,12 +72,16 @@
                                 </div>
                                 </div>
                             </div>
-<div class="col-md-7">
+                            <div class="col-md-7">
+                                <div class="form-group row">
+                                <label for="" class="col-md-5 col-sm-3 col-form-label pr-0 mr-md-1"> Add 360 Image</label>
+                                <div class="col-md-6 col-sm-9 pl-3 pl-md-4 ">
+                                     <input data-id="userstatus" class=" form-control radio userstatus btn_lg btn_green my-lg-3 mx-md-2 mx-lg-0" id="userstatus" type="checkbox" data-toggle="toggle" data-on="Active" checked data-off="Inactive"  data-size="small" data-onstyle="primary">
+                                </div>
+                                </div>
+                            </div>
 
-                            Add 360 Image
-                    <input data-id="userstatus" class=" form-control radio userstatus btn_lg btn_green my-lg-3 mx-md-2 mx-lg-0" id="userstatus" type="checkbox" data-toggle="toggle" data-on="Active" checked data-off="Inactive"  data-size="small" data-onstyle="primary">
-                        </div>
-                    </div>
+ </div>
  </div>
                     <div class="col-lg-3 text-right">
                     <!--<button class="btn_lg btn_none">Add 360 Image</button>-->
