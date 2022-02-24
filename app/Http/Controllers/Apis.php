9116d7106189e663c52a0aff1c6da45e65aa1e24
1986dheeraj <93111832+1986dheeraj@users.noreply.github.com>
2022-02-24 12:56:47 +0530

    api for cemetery and plot list

---
+++ app/Http/Controllers/Apis.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Cemetery;
+use App\Models\Plot;
+
+class Apis extends Controller
+{
+    
+   
+ 
+   public function cemeterycreate(Request $request)
+   {
+       
+      
+ $validated = $request->validate([
+        'cemetery_name' => 'required',
+        'feature' => 'required',
+            'public' => 'required',
+    ]);
+
+       $cemetery = new Cemetery;
+               $cemetery->cemetery_name = $request->cemetery_name;
+
+$cemetery->country = $request->country;
+               $cemetery->state = $request->state;
+               $cemetery->city = $request->city;
+               $cemetery->address = $request->address;
+               $cemetery->location = $request->location;
+               $cemetery->zip = $request->zip;
+               $cemetery->cemetery_desc = $request->cemetery_desc;
+               $cemetery->locationtitle1 = $request->locationtitle1;
+               $cemetery->locationtitle2 = $request->locationtitle2;
+               $cemetery->locationtitle3 = $request->locationtitle3;
+               $cemetery->locationtitle4 = $request->locationtitle4;
+               $cemetery->locationtitle5 = $request->locationtitle5;
+               $cemetery->locationtitle6 = $request->locationtitle6;
+               $cemetery->feature = $request->feature;
+               $cemetery->public = $request->public;
+
+               $cemetery->save();
+
+
+return response()->json($cemetery, 201);
+   
+   }
+
+    public function plotcreate(Request $request)
+   {
+       
+       $validated = $request->validate([
+        'cemetery_id' => 'required',
+        'feature' => 'required',
+          
+    ]);
+
+       $plot = new Plot;
+               $plot->cemetery_id = $request->cemetery_id;
+               $plot->plotstatus = $request->plotstatus;
+               $plot->plottype1 = $request->plottype1;
+               $plot->plottype2 = $request->plottype2;
+               $plot->plottype3 = $request->plottype3;
+               $plot->plot_public = $request->plot_public;
+               $plot->minprice = $request->minprice;
+               $plot->maxprice = $request->maxprice;
+                $plot->price = $request->price;
+               $plot->internal_notes = $request->internal_notes;
+               $plot->locationtitle1 = $request->locationtitle1;
+               $plot->locationtitle2 = $request->locationtitle2;
+               $plot->locationtitle3 = $request->locationtitle3;
+               $plot->locationtitle4 = $request->locationtitle4;
+               $plot->locationtitle5 = $request->locationtitle5;
+               $plot->locationtitle6 = $request->locationtitle6;
+               $plot->feature = $request->feature;
+               $plot->save();
+
+
+return response()->json($plot, 201);
+   
+   }
+   
+      public function getallcemeteries()
+        {
+          
+         return Cemetery::all();
+        }
+
+
+
+        public function getallcemeteryshow($id)
+    {
+        return Cemetery::find($id);
+    }
+    
+     public function getallplots($cemeteryid)
+    {
+
+        
+$plots =  DB::table('plot')
+        ->join('cemetery', 'cemetery.ID', '=', 'plot.cemetery_id')
+        ->select('plot.*', 'cemetery.cemetery_name')
+         ->where('plot.cemetery_id', $cemeteryid) ->get();
+
+
+
+        return $plots;
+
+    }
+   public function getplotshow($cemeteryid,$plotid)
+    {
+       
+
+        $plots =  DB::table('plot')
+        ->join('cemetery', 'cemetery.ID', '=', 'plot.cemetery_id')
+        ->select('plot.*', 'cemetery.cemetery_name')
+         ->where('plot.cemetery_id', $cemeteryid) ->where('plot.id', $plotid) ->get();
+        return $plots;
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Apis;
+use App\Models\Plot;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get('/first-api', [Apis::class,'firstapi']);
+Route::post('/cemetery-create', [Apis::class,'cemeterycreate']);
+Route::post('/plot-create', [Apis::class,'plotcreate']);
+
+Route::get('cemetery/{id}/', [Apis::class, 'getallplots']);
+Route::get('cemetery/{cemeteryid}/plot/{plotid}', [Apis::class, 'getplotshow']);
+Route::get('/cemeteries', [Apis::class, 'getallcemeteries']);
+Route::get('/cemeteries/{id}', [Apis::class, 'getallcemeteryshow']);
