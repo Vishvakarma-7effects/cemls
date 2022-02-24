1391cd87f65306650392a919e5d9bbae219b8b55
1986dheeraj <93111832+1986dheeraj@users.noreply.github.com>
2022-02-24 15:01:28 +0530

    search by cemetery name or user name or email on mailbox listing

--- app/Http/Controllers/MailboxController.php
+++ app/Http/Controllers/MailboxController.php
@@ -19,18 +19,48 @@ class MailboxController extends Controller
      */
     public function index()
     {
-      
-  $cemetery360_inboxs = Mailinbox::orderBy('id', 'DESC')->paginate(10);
+        $cemetery360_inboxs = Mailinbox::orderBy('id', 'DESC')->paginate(10);
+        if (request('term')) 
+        {
+                $cemetery360_inboxs = DB::table('cemetery360_inbox')
+               
+                ->where('sender_name','like',"%".request('term')."%")
+                ->orWhere('sender_email','like',"%".request('term')."%")
+                ->orWhere('inbox_title','like',"%".request('term')."%")
+                ->orWhere('inbox_title','like',"%".request('term')."%")
+                ->paginate(10);
+
+        }
     
         return View('admin.mailbox')->with('cemetery360_inboxs', $cemetery360_inboxs);
-    }
+        }
 
-    public function destroy(Mailinbox $mailinbox)
-    {
+    public function destroy($id,Mailinbox $mailinbox)
+     {
+
+        $mailinbox = Mailinbox::where('id',$id)->delete();
+      return redirect()->route('mailbox.index')
+                     ->with('success','Mailbox data deleted successfully');
+        /*
         $mailinbox->delete();
 
         return redirect()->route('mailbox.index')
-                        ->with('success','Mailbox data deleted successfully');
+                     ->with('success','Mailbox data deleted successfully');
+
+                     */
     }
+    // {
+        
+    //   $mailbox = Mailbox::where(['id'=>$mailbox->id])->delete();
   
+    
+    // return redirect()->to('mailbox.index')->with('success', 'Mailbox Deleted Succesfully');;
+
+    // }
+    public function mailboxdelete(Mailinbox $mailinbox)
+    { 
+        $mailinbox = Mailinbox::where('id',$id)->delete();
+        return Redirect::to('mailbox.index')->with('message', 'Mailinbox Deleted Sucessfully');
+    }
+
 }
