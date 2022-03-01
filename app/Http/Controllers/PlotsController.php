bb56f6c1b9d7f8f84ebf7034a8f363aca7ac3703
1986dheeraj <93111832+1986dheeraj@users.noreply.github.com>
2022-03-01 16:58:49 +0530

    Plot filter issue resolved

--- app/Http/Controllers/PlotsController.php
+++ app/Http/Controllers/PlotsController.php
@@ -20,13 +20,14 @@ class PlotsController extends Controller
 		public function index(){
 					abort_if(Gate::denies('plot_list'), Response::HTTP_FORBIDDEN, '403 Forbidden');
 
-					 $plots = Plot::orderBy('id', 'DESC')->paginate(10);
-       $burialsplots = Plot::orderBy('id', 'DESC')->where('plottype1','Burials')->paginate(10);
-       $cremationsplots= plot::orderBy('id', 'DESC')->where('plottype1','Cremation')->paginate(10);
-       $abovegroundplots= plot::orderBy('id', 'DESC')->where('plottype2','Above Ground')->paginate(10);
-       $belowgroundplots= plot::orderBy('id', 'DESC')->where('plottype2','Below Ground')->paginate(10);
-       $Indoorplots= plot::orderBy('id', 'DESC')->where('plottype3','Indoor')->paginate(10);
-       $Outdoorplots= plot::orderBy('id', 'DESC')->where('plottype3','Outdoor')->paginate(10);
+					 $plots = Plot::orderBy('id', 'DESC')->paginate(5);
+        $Indoorplots= plot::orderBy('id', 'DESC')->where('plottype1','Indoor')->paginate(5);
+       $Outdoorplots= plot::orderBy('id', 'DESC')->where('plottype1','Outdoor')->paginate(5);
+       $abovegroundplots= plot::orderBy('id', 'DESC')->where('plottype2','Above Ground')->paginate(5);
+       $belowgroundplots= plot::orderBy('id', 'DESC')->where('plottype2','Below Ground')->paginate(5);
+      
+       $burialsplots = Plot::orderBy('id', 'DESC')->where('plottype3','Burial')->paginate(5);
+       $cremationsplots= plot::orderBy('id', 'DESC')->where('plottype3','Cremation')->paginate(5);
         if (request('term')) {
 
         $plots =  DB::table('plot')
