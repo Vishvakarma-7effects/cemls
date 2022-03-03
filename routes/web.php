f39eb0fa211ecd2bb5e46d69f0b1b566db584fbd
1986dheeraj <93111832+1986dheeraj@users.noreply.github.com>
2022-03-03 21:18:21 +0530

    route for tag and location

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,8 @@ use App\Http\Controllers\InboxController;
 use App\Http\Controllers\PlotsController;
 use App\Http\Controllers\MailboxController;
 use App\Http\Controllers\Apis;
+use App\Http\Controllers\LocationController;
+use App\Http\Controllers\TagsController;
 
 
 /*
@@ -193,3 +195,11 @@ Route::post('fetchstates',[CemeteryController::class, 'fetchstates']);
 Route::post('cemeteries/get-destroycemeteryimage', [CemeteryController::class, 'destroycemeteryimage'])->middleware('auth');
 Route::get('cemetery/cemeteryListViewNew', [CemeteryController::class, 'cemeteryListViewNew']);
 Route::get('/getallcemeteries', [Apis::class, 'getallusercemeteries']);
+Route::get('location/delete/{id}', [LocationController::class, 'destroy'])->middleware('auth');
+
+Route::resource('tag', TagsController::class)->middleware('auth');
+Route::get('tag/edit/{id}', [TagsController::class, 'edit'])->middleware('auth');
+Route::post('updatetag', [TagsController::class, 'update'])->name('tag.update')->middleware('auth');
+Route::get('tag/delete/{id}', [TagsController::class, 'destroy'])->middleware('auth');
+
+Route::post('plots/get_location', [PlotsController::class, 'get_location'])->middleware('auth');
